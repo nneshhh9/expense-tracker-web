96d079ce68818986dab45d6cac1298061d111efc
Vlad Kozhuhov <kozhuhov.v@konsom.ru>
2024-04-14 21:10:24 +0500

    add project

---
+++ expense-tracker-api/expense-tracker-api/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace expense_tracker_api.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
