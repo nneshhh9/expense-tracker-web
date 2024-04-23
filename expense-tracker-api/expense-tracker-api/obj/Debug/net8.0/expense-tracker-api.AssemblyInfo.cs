33c0c4ae84bc43429612cab744f36c3d6d7793c2
Vlad Kozhuhov <kozhuhov.v@konsom.ru>
2024-04-23 23:12:06 +0500

    добавление сущностей в models, настройка подключения, добавление nuget EF

--- expense-tracker-api/expense-tracker-api/obj/Debug/net8.0/expense-tracker-api.AssemblyInfo.cs
+++ expense-tracker-api/expense-tracker-api/obj/Debug/net8.0/expense-tracker-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("expense-tracker-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96d079ce68818986dab45d6cac1298061d111efc")]
 [assembly: System.Reflection.AssemblyProductAttribute("expense-tracker-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("expense-tracker-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
